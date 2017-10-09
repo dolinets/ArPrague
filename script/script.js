dd561c9b079d779134695b2c33b91ce8ec41018f
Yulia Dolinets <dolinets123@gmail.com>
2017-10-09 08:26:11 +0300

    final test work

--- script/script.js
+++ script/script.js
@@ -8,5 +8,55 @@ $(document).ready(function(){
 				}
 		});
 	});
+
+	var valActiveCheckbox = [];
+
+	$("button.button-orange").click(function () {				
+		valActiveCheckbox = [];		
+		var activeCheckbox = $(this).parents("ul.dropdown-menu-custom").find("input:checked"); 
+		$(activeCheckbox).each (
+			function(){
+				valActiveCheckbox.push($(this).val());
+			}
+		);
+		console.log(valActiveCheckbox);
+		addActiveCheckbox (valActiveCheckbox, $(this).parents(".button-selected-container"));
+		console.log($(this).parents(".button-selected-container"));
+   	});
+
+   	$("button.closeFilters").click(function () {	
+   		valActiveCheckbox = [];
+   		addActiveCheckbox (valActiveCheckbox,$(this).parents(".button-selected-container"));
+   		$(this).parent().find("input").prop("checked", false);
+   	});
+
+   	$(".filter-value").click(function () {
+   		valActiveCheckbox.push($(this).text());
+   		addActiveCheckbox (valActiveCheckbox, $(this).parents(".button-selected-container"));
+   		console.log(valActiveCheckbox);
+   	});
+
+	function addActiveCheckbox (valActiveCheckbox, container){
+		var selectedFilters = $(container).find("button.selectedFilters");
+		var closeFilters = $(container).find("button.closeFilters");
+		if (valActiveCheckbox.length > 0) {
+			$(selectedFilters).removeClass("hidden");
+			$(closeFilters).removeClass("hidden");
+		} else {
+			$(selectedFilters).addClass("hidden");
+			$(closeFilters).addClass("hidden");
+		}
+
+   		if (valActiveCheckbox.length == 1){
+   			$(selectedFilters).text(valActiveCheckbox[0]);
+   		}
+
+   		if (valActiveCheckbox.length > 1){
+   			$(selectedFilters).text("("+valActiveCheckbox.length+")");
+   		}
+
+   	}
 });
+
+
 
